4df3466cafc27c2b0400c957e2e4471e02bedc1d
loomz <loomz@163.com>
2022-10-10 10:13:16 +0800

    修改把get_data_csv2()改成get_data_csv()

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     # pytest.main(['-s', 'test_test_wm.py', '--html=pytest3.html'])
     # pytest.main(['-s','pytest_test_wm.py','-v', '--html=pytest6.html'])
 
-    # 测试当前文件中用例且生成报告./report,每次运行之前先清理报告所有的目录
+    # 测试当前文件中用例且生成报告./report,每次运行之前先清理报告所有的目录bbbvssssssssss
     pytest.main(['-sv', 'pytest_test_wm.py', __file__, '--alluredir', './allure-report', '--clean-alluredir'])
     # 查看执行，运行allure serve 报告所在目录
     # os.system('allure serve ./allure-report')
